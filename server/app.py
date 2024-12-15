cc47d53b9bac6a3d0911371d4099084aa5178cf5
sbabadag <cesbabadag@gmail.com>
2024-12-15 21:17:44 +0300

    Fix Heroku deployment to start Flask server

--- server/app.py
+++ server/app.py
@@ -27,5 +27,5 @@ def recommend():
     return jsonify(recommendations)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)  # Disable debug mode for production
---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require('path')
+const cool = require('cool-ascii-faces')
+
+const port = process.env.PORT || 5006
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+})
+
+app.get('/cool', (req, res) => {
+  res.send(cool())
+})
+
+app.get('/times', (req, res) => {
+  const times = process.env.TIMES || 5
+  let result = ''
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  res.send(result)
+})
+
+const server = app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
+
+process.on('SIGTERM', async () => {
+  console.log('SIGTERM signal received: gracefully shutting down')
+  if (server) {
+    server.close(() => {
+      console.log('HTTP server closed')
+    })
+  }
+})
